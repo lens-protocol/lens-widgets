c6fe8722c86fd48212ff997cdce659e2c104a82e
benbaessler <mail@benbaessler.com>
2023-12-06 18:57:21 +0000

    add mumbai testnet support

--- lens-widgets-react/src/graphql/client.ts
+++ lens-widgets-react/src/graphql/client.ts
@@ -1,8 +1,11 @@
 import { createClient } from 'urql'
 
+const network: 'polygon' | 'mumbai' = 'polygon';
+
 const API_URL = 'https://api-v2.lens.dev'
+const TESTNET_API_URL = 'https://api-v2-mumbai-live.lens.dev'
 
 /* creates the API client */
 export const client = createClient({
-  url: API_URL
+  url: network == 'mainnet' ? API_URL : TESTNET_API_URL,
 })
